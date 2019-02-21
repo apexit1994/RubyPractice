b057e2b3571d8115eb81020d5185816459ddecb9
Apexit Dhandav <adhandav@localhost.localdomain>
2019-02-21 12:12:53 +0530

    markups classification

--- readpdf.rb
+++ readpdf.rb
@@ -1,18 +1,55 @@
 require 'pdf-reader'
 
-#get Data from last page of document
-file = '/home/adhandav/Downloads/CNT2677568.pdf'
+class MarkupSummaryData
 
-if File.exist?(file)
-    reader = PDF::Reader.new(file)
-    pc = reader.page_count
-    page   = reader.page(pc)
-    data = page.text
+    def getTotalComments(markupSummary)
 
-    #search for particular string
-    # positions = data.gsub(/Total Comments:/).map { Regexp.last_match.begin(0) }
-    # puts positions
+        array=markupSummary.split("\s")
+        $i=0;
+        $length = array.length
+        while $i < $length-3 do
+            if (array[$i] == "Total") && (array[$i+1] == "Comments:")
+                return "Total Comments :"+array[$i+2]
+            end
+            $i+=1
+        end
+    end
 
-    puts data.split("\s")
+    def getMarkupList(markupSummary)
+
+        array=markupSummary.split("\s")
+        $i=0;
+        $length = array.length
+        while $i < $length-3 do
+            if (array[$i] == "ID") && (array[$i+1] == "Type") && (array[$i+2] == "Comments")
+                
+            end
+            $i+=1
+        end
+        
+    end
+
+    def extractPDFText
+        #get Data from last page of document
+        file = '/home/adhandav/Downloads/CNT2677568.pdf'
+
+        if File.exist?(file)
+            reader = PDF::Reader.new(file)
+            pc = reader.page_count
+            page   = reader.page(pc)
+            data = page.text
+            puts getTotalComments(data)
+            getMarkupList(data)
+        else
+            puts "File does not exists"
+            
+        end
+    end
 
 end
+
+obj = MarkupSummaryData.new
+obj.extractPDFText
+
+
+
