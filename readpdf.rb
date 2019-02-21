044dca046eab05898dfdd836340ea568c0d7f315
Apexit Dhandav <adhandav@localhost.localdomain>
2019-02-21 10:25:26 +0530

    pdfreader updated

--- readpdf.rb
+++ readpdf.rb
@@ -1,12 +1,18 @@
 require 'pdf-reader'
 
 #get Data from last page of document
-reader = PDF::Reader.new('/home/adhandav/Downloads/CNT2677568.pdf')
-pc = reader.page_count
-page   = reader.page(pc)
-data = page.text
+file = '/home/adhandav/Downloads/CNT2677568.pdf'
 
-#search for particular string
-positions = data.gsub(/on/).map { Regexp.last_match.begin(0) }
+if File.exist?(file)
+    reader = PDF::Reader.new(file)
+    pc = reader.page_count
+    page   = reader.page(pc)
+    data = page.text
 
-puts positions
+    #search for particular string
+    # positions = data.gsub(/Total Comments:/).map { Regexp.last_match.begin(0) }
+    # puts positions
+
+    puts data.split("\s")
+
+end
