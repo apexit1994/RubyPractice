c3fa0503567150b236d3ff4b16cac2cff9e4b294
Apexit Dhandav <adhandav@localhost.localdomain>
2019-02-22 15:15:58 +0530

    seperating comments using function

--- readpdf.rb
+++ readpdf.rb
@@ -2,102 +2,113 @@ require 'pdf-reader'
 
 class MarkupSummaryData
 
-    attr_accessor :type, :comments, :username, :time
-    def initialize( type, comments, username, time)
-        # local variables shadow the reader methods
-        @type = type
-        @comments = comments
-        @username = username
-        @time = time
+  attr_accessor :type, :comments, :username, :time
+
+  def initialize(type, comments, username, time)
+    # local variables shadow the reader methods
+    @type = type
+    @comments = comments
+    @username = username
+    @time = time
+  end
+
+  def getTotalNumberOfComments(markupSummary)
+    array = markupSummary.split("\s")
+    $i = 0;
+    $length = array.length
+    while $i < $length - 3 do
+      if (array[$i] == "Total") && (array[$i + 1] == "Comments:")
+        return "Total Comments :" + array[$i + 2]
+      end
+      $i += 1
+    end
+  end
+
+  def getComments(array, index)
+
+    comments = username = ""
+
+    while (array[index] != "-" && !array[index + 1].match(/M[rs]+[s]*/))
+      comments += array[index]
+      index += 1
     end
 
-    def getTotalNumberOfComments(markupSummary)
-        array=markupSummary.split("\s")
-        $i=0;
-        $length = array.length
-        while $i < $length-3 do
-            if (array[$i] == "Total") && (array[$i+1] == "Comments:")
-                return "Total Comments :"+array[$i+2]
-            end
-            $i+=1
-        end
+    index += 1
+
+    while (array[index] != "on")
+      username += array[index]
+      index += 1
+    end
+    index += 1
+    time = array[index]
+
+    return comments, username, time, index
+
+  end
+
+  def getMarkupList(markupSummary)
+
+    index = 0, id = 1, type, comments = "", username = "", time, markuplist = []
+    array = markupSummary.split("\s")
+    $i = 0;
+    $length = array.length
+    while $i < $length - 3 do
+      if (array[$i] == "ID") && (array[$i + 1] == "Type") && (array[$i + 2] == "Comments")
+        index = $i
+      end
+      $i += 1
     end
 
-    def getMarkupList(markupSummary)
+    index += 3
 
-        index=0,id=1,type,comments="",username="",time, markuplist=[]
-        array=markupSummary.split("\s")
-        $i=0;
-        $length = array.length
-        while $i < $length-3 do
-            if (array[$i] == "ID") && (array[$i+1] == "Type") && (array[$i+2] == "Comments")
-                index=$i
-            end
-            $i+=1
-        end
+    while (index < $length)
+      if (id == array[index].to_i)
+        index += 1
+      end
 
-        index+=3
-        
-        while (index < $length)
-            if(id == array[index].to_i)
-                index+=1
-            end
+      type = array[index]
+      index += 1
 
-            type=array[index]
-            index+=1
+      comments, username, time, index = getComments(array, index)
 
-            while(array[index] != "-" && !array[index + 1].match(/M[rs]+[s]*/))
-               comments+=array[index]
-               index+=1 
-            end
-
-            index+=1
-
-            while(array[index] != "on")
-                username += array[index]
-                index+=1
-            end
-            index+=1
-            time = array[index]
-
-            markuplist.push(MarkupSummaryData.new type, comments, username, time)
-            index+=1
-            id+=1
-            type="",comments="",username="",time=""
-        end
-
-        return markuplist
-        
+      markuplist.push(MarkupSummaryData.new type, comments, username, time)
+      index += 1
+      id += 1
+      type = "", comments = "", username = "", time = ""
     end
 
-    def extractPDFText
-        #get Data from last page of document
-        file = '/home/adhandav/Downloads/CNT2677568 (2).pdf'
+    return markuplist
 
-        if File.exist?(file)
-            reader = PDF::Reader.new(file)
-            pc = reader.page_count
-            page   = reader.page(pc)
-            data = page.text
-            puts getTotalNumberOfComments(data)
-            datalist = getMarkupList(data)
+  end
 
-            for i in datalist
-                puts i.type
-                puts i.comments
-                puts i.username
-                puts i.time
-                puts ""
-            end
-        else
-            puts "File does not exists"
-            
-        end
+  def extractPDFText
+    #get Data from last page of document
+    file = '/home/adhandav/Downloads/CNT2677568 (2).pdf'
+
+    if File.exist?(file)
+      reader = PDF::Reader.new(file)
+      pc = reader.page_count
+      page = reader.page(pc)
+      data = page.text
+      puts getTotalNumberOfComments(data)
+      datalist = getMarkupList(data)
+
+      for i in datalist
+        puts i.type
+        puts i.comments
+        puts i.username
+        puts i.time
+        puts ""
+      end
+    else
+      puts "File does not exists"
+
     end
+  end
 
 end
 
-obj = MarkupSummaryData.new 1,2,3,4
+obj = MarkupSummaryData.new 1, 2, 3, 4
 obj.extractPDFText
 
 
